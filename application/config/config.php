b9122fbde412405d14d602c2dc435f0a7b462077
Yan Carlos Marin Osorio <yancarlosmarinosorio@gmail.com>
2020-09-12 10:59:25 -0500

    validating localhost for base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "http://".$_SERVER['HTTP_HOST']."/loteria_quindio";
+if($_SERVER['HTTP_HOST'] == 'localhost:8888'){
+	$config['base_url'] = "http://".$_SERVER['HTTP_HOST']."/loteria_quindio";
+}else{
+	$config['base_url'] = "http://".$_SERVER['HTTP_HOST'];
+}
 
 /*
 |--------------------------------------------------------------------------
