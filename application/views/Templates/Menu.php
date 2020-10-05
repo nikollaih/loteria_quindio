ac81d5814bf4e8e194055b5990b5f4a9795d72cb
Nikollai Hernandez G <nhernandez@heinsohn.com.co>
2020-10-05 13:53:04 -0500

    Factura

--- application/views/Templates/Menu.php
+++ application/views/Templates/Menu.php
@@ -5,23 +5,23 @@
         <div id="sidebar-menu" class="slimscroll-menu">
             <ul class="metismenu" id="menu-bar">
                 <li class="menu-title">Menú</li>
-                <li>
-                    <a href="javascript: void(0);">
-                        <i data-feather="users"></i>
-                        <span> Usuarios </span>
-                        <span class="menu-arrow"></span>
-                    </a>
-
-                    <ul class="nav-second-level" aria-expanded="false">
-                        <li>
-                            <a href="<?= base_url() . 'Usuarios/List'; ?>">Ver Todos</a>
-                        </li>
-                    </ul>
-                </li>
-
+                
                 <?php
                     if(is_admin()){
                 ?>
+                    <li>
+                        <a href="javascript: void(0);">
+                            <i data-feather="users"></i>
+                            <span> Usuarios </span>
+                            <span class="menu-arrow"></span>
+                        </a>
+
+                        <ul class="nav-second-level" aria-expanded="false">
+                            <li>
+                                <a href="<?= base_url() . 'Usuarios/List'; ?>">Ver Todos</a>
+                            </li>
+                        </ul>
+                    </li>
                     <li>
                         <a href="<?= base_url() . 'Draws'; ?>">
                             <i data-feather="star"></i>
