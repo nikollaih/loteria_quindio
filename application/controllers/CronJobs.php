9b30009e3ecdbbb2829a9e0931ec686b2be3ef6b
Nikollai Hernandez G <nikollaihernandez@gmail.com>
2021-04-27 18:27:18 -0500

    Ajusted  cron jobs

--- application/controllers/CronJobs.php
+++ application/controllers/CronJobs.php
@@ -15,36 +15,38 @@ class CronJobs extends Application_Controller {
     // Run every friday at 00:00
 	public function subscriber_to_purchase()
 	{
-        // Get the subscriber list
-        $subscriber = $this->Subscriber->get_subscribers();
-        // Get the current active draw
-        $draw = $this->Draw->get_active_draw();
+        if(date("N") == 5){
+            // Get the subscriber list
+            $subscriber = $this->Subscriber->get_subscribers();
+            // Get the current active draw
+            $draw = $this->Draw->get_active_draw();
 
-        // Validate if subscriber is an array and different than false
-        if(is_array($subscriber) && $subscriber != false){
-            foreach ($subscriber as $s) {
-                // Create the new purchase array
-                $data["id_user"] = $s["id_user"];
-                $data["id_draw"] = $draw["id"];
-                $data["number"] = $s["number"];
-                $data["serie"] = $s["serie"];
-                $data["parts"] = $draw["fractions_count"];
-                $data["purchase_status"] = $s["purchase_status"];
-                $data["created_at"] = date("Y-m-d h:m:s");
+            // Validate if subscriber is an array and different than false
+            if(is_array($subscriber) && $subscriber != false){
+                foreach ($subscriber as $s) {
+                    // Create the new purchase array
+                    $data["id_user"] = $s["id_user"];
+                    $data["id_draw"] = $draw["id"];
+                    $data["number"] = $s["number"];
+                    $data["serie"] = $s["serie"];
+                    $data["parts"] = $draw["fractions_count"];
+                    $data["purchase_status"] = $s["purchase_status"];
+                    $data["created_at"] = date("Y-m-d h:m:s");
 
-                // Validate if it exists any active purchase with the same data
-                $validate_purchase = $this->Purchase->validate_purchase($data);
-                if(!is_array($validate_purchase) && !$validate_purchase){
-                    // Save the new purchase
-                    $result_purchase = $this->Purchase->set_purchase($data);
-                    // If purchase was created successfully then it will update the remaining subscriber amount
-                    if(is_array($result_purchase)){
-                        add_loto_punto();
-                        $result_subscriber = $this->Subscriber->update_subscriber(array("subscriber_remaining_amount" => $s["subscriber_remaining_amount"] - 1, "id_subscriber" => $s["id_subscriber"]));
-                        // If there is any error while updating the subscriber then the purchase will be removed
-                        if(!$result_subscriber){
-                            add_loto_punto("substract");
-                            $this->Purchase->delete_purchase($result_purchase["id_purchase"]);
+                    // Validate if it exists any active purchase with the same data
+                    $validate_purchase = $this->Purchase->validate_purchase($data);
+                    if(!is_array($validate_purchase) && !$validate_purchase){
+                        // Save the new purchase
+                        $result_purchase = $this->Purchase->set_purchase($data);
+                        // If purchase was created successfully then it will update the remaining subscriber amount
+                        if(is_array($result_purchase)){
+                            add_loto_punto();
+                            $result_subscriber = $this->Subscriber->update_subscriber(array("subscriber_remaining_amount" => $s["subscriber_remaining_amount"] - 1, "id_subscriber" => $s["id_subscriber"]));
+                            // If there is any error while updating the subscriber then the purchase will be removed
+                            if(!$result_subscriber){
+                                add_loto_punto("substract");
+                                $this->Purchase->delete_purchase($result_purchase["id_purchase"]);
+                            }
                         }
                     }
                 }
@@ -62,7 +64,9 @@ class CronJobs extends Application_Controller {
     // Set the lotto points to 0
     // Run after finished the current draw
     function clear_lotto_points(){
-        $this->Usuario->clear_lotto_points();
+        if(date("N") == 4){
+            $this->Usuario->clear_lotto_points();
+        }
     }
 
     
