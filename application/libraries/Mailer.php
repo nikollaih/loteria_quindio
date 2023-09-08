c847c120a89d9af4f51ea3904229c897d5aacea3
Nikollai Hernandez G <nikollaihernandez@gmail.com>
2023-09-08 11:42:21 -0500

    Mailer port

--- application/libraries/Mailer.php
+++ application/libraries/Mailer.php
@@ -18,7 +18,7 @@ class Mailer {
     $config = array(
         'protocol'  => 'smtp',
         'smtp_host' => 'ssl://smtp.gmail.com',
-        'smtp_port' => 465,
+        'smtp_port' => 587,
         'smtp_user' => 'loteriadelquindiosoporte@gmail.com',
         'smtp_pass' => '@Loteria123',
         'mailtype'  => 'html',
